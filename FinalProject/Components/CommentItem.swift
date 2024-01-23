2b0ad96bb564e301d8e39224ff6cc2afeccb9e5d
sofoetno <sopho.machurishvili@gmail.com>
2024-01-23 16:41:00 +0400

    add comment time, add like on feed and on comments

---
+++ FinalProject/Components/CommentItem.swift
@@ -0,0 +1,18 @@
+//
+//  CommentItem.swift
+//  FinalProject
+//
+//  Created by Sofo Machurishvili on 23.01.24.
+//
+
+import SwiftUI
+
+struct CommentItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CommentItem()
+}
